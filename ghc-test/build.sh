fa66f8fbc6b627d185825483af5a6eed298fe580
Ben Gamari <bgamari.foss@gmail.com>
2013-12-27 14:35:56 -0500

    Parametrize -O level

--- ghc-test/build.sh
+++ ghc-test/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+opt=0
+
 name=llvm
 cabal clean
-cabal configure --ghc-options='-O0 -v -fllvm' --disable-library-profiling --enable-shared
+cabal configure --ghc-options="-O$opt -v -fllvm" --disable-library-profiling --enable-shared
 cabal build
 cp dist/build/libHSlibtest*.so lib-${name}.so
 cp dist/build/test/test test-${name}
@@ -11,7 +13,7 @@ objdump -Dj .text lib-${name}.so > lib-${name}.text
 
 name=ncg
 cabal clean
-cabal configure --ghc-options='-O0 -v' --disable-library-profiling --enable-shared
+cabal configure --ghc-options="-O$opt -v" --disable-library-profiling --enable-shared
 cabal build
 cp dist/build/libHSlibtest*.so lib-${name}.so
 cp dist/build/test/test test-${name}
