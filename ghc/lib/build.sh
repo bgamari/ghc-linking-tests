4f1b0c6dbee7c7c29d96cc2211bc5daecd7dab73
Ben Gamari <bgamari.foss@gmail.com>
2013-12-27 12:45:02 -0500

    Fix build script

--- ghc/lib/build.sh
+++ ghc/lib/build.sh
@@ -5,8 +5,8 @@ cabal clean
 cabal configure --ghc-options='-v -fllvm' --disable-library-profiling --enable-shared
 cabal build
 cp dist/build/libHSlibtest*.so lib-${name}.so
-objdump -xRrtTd lib-$name > lib-${name}.objdump
-objdump -Dj .text lib-$name > lib-${name}.text
+objdump -xRrtTd lib-${name}.so > lib-${name}.objdump
+objdump -Dj .text lib-${name}.so > lib-${name}.text
 
 name=ncg
 cabal clean
